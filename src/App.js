e31780f73c1fd1bccfcc29fe13d980a7277e22f9
Dawid Leszczyński <95439611+Comand94@users.noreply.github.com>
2022-06-10 01:17:06 +0200

    Fetch songs

--- src/App.js
+++ src/App.js
@@ -1,45 +1,59 @@
 import "./App.css"
 import Song from "./components/Song"
-import React, { useEffect, useState } from "react"
+import React, { useState, useMemo } from "react"
 import { nanoid } from "nanoid"
 
-function App() {
-    const [numberOfSongs, setNumberOfSongs] = useState(5)
-    const [wordsData, setWordsData] = useState([])
+export default function App() {
+    const [wordsData, setWordsData] = useState([]) // data[0].word
+    const [numberOfSongs, setNumberOfSongs] = useState(5) // between 5 and 12
+    const [fetchingWordsProgress, setFetchingWordsProgress] = useState(0) // for progress bar
 
-    const songs = wordsData.map(word => {
-        return (
-            <Song
-                key={nanoid()}
-                word={word}
-            />
-        )
-    })
-    
-    // on input change
+    // only update Song components when wordsData is updated as well
+    const songsHTML = useMemo(() =>
+        wordsData.map(word => {
+            return (
+                <Song
+                    key={nanoid()}
+                    word={word}
+                />
+            )
+        }
+    ), [wordsData])
+
+    const wordsProgressHTML = (
+        <h2 className="App-fetching-text" >
+            Fetching words {fetchingWordsProgress}/{numberOfSongs}
+        </h2 >
+    )
+  
+    // on input box change
     function changeNumberOfSongs(event) {
         let val = event.target.value
 
         // validation
         if (val < 5) val = 5
-        else if (val > 20) val = 20
+        else if (val > 12) val = 12
 
         setNumberOfSongs(val)
     }
 
     // on button press
     async function randomizeWords() {
-        const newWordsData = []
+        if (fetchingWordsProgress === 0) { // don't fetch again until finished fetching
+            const newWordsData = []
 
-        for (let i = 0; i < numberOfSongs; i++) {
-            let data = ""
-            do {
-                data = await fetch("https://random-words-api.vercel.app/word")
-                    .then(res => res.json()).then(data => data[0].word)
-            } while (newWordsData.includes(data)) // make sure the word isn't repeated
-            newWordsData.push(data) // push the new word into the array
+            for (let i = 0; i < numberOfSongs; i++) {
+                let data = ""
+                do {
+                    data = await fetch("https://random-words-api.vercel.app/word")
+                        .then(res => res.json()).then(data => data[0].word)
+                } while (newWordsData.includes(data)) // make sure the word isn't repeated
+                newWordsData.push(data) // push the new word into the array
+                setFetchingWordsProgress(i)
+            }
+            setWordsData(newWordsData.sort())
+            setFetchingWordsProgress(0)
         }
-        setWordsData(newWordsData.sort())
     }
 
     return (
@@ -48,16 +62,15 @@ function App() {
                 <h1>React Playlist Randomizer</h1>
                 <div className="App-form">
                     <input className="App-input" onChange={changeNumberOfSongs}
-                        type="number" min="5" max="20"></input>
+                        value={numberOfSongs}
+                        type="number" min="5" max="12"></input>
                     <button className="App-button" onClick={randomizeWords}>Randomize Songs</button>
                 </div>
             </div>
 
             <div className="App-songs">
-                {songs}
+                {fetchingWordsProgress !== 0 ? wordsProgressHTML : songsHTML}
             </div>
         </div>
     )
 }
-
-export default App
--- src/components/Song.js
+++ src/components/Song.js
@@ -1,11 +1,45 @@
-import React from "react"
 import "./Song.css"
+import React, { useEffect, useState } from "react"
 
+// Song takes word as prop and fetches it's own recording data based on that
 export default function Song(props) {
+    const [artistName, setArtistName] = useState("")
+    const [albumName, setAlbumName] = useState("")
+    const [songName, setSongName] = useState("")
+
+    const recordingHTML = (
+        <h5 className="Song-recording">
+            {songName !== "" ?
+                `${artistName} | ${albumName} | ${songName}`
+                : "No recording found!"}
+        </h5>
+    )
+
+    // fetch data on creation only
+    useEffect(() => {
+        async function getSongInfo() {
+            const data =
+                await fetch(`http://musicbrainz.org/ws/2/recording?query=${props.word}&limit=1&fmt=json`)
+                    .then(res => res.json())
+
+            if (data.recordings[0] !== undefined) { // recording found
+                setSongName(data.recordings[0].title)
+                setArtistName(data.recordings[0]["artist-credit"][0].name)
+                setAlbumName(data.recordings[0]["releases"][0].title)
+            }
+            else { // recording not found - "" will be interpreted as not found
+                setSongName("")
+                setArtistName("")
+                setAlbumName("")
+            }
+        }
+        getSongInfo() // call once
+    }, [])
+
     return (
         <div className="Song">
             <h3>--- {props.word} ---</h3>
-            <h4 className="Song-recording">Billy Joel - The Stranger - She's Always A Woman</h4>
+            {recordingHTML}
         </div>
     )
 }
